076102155aedec561c6af51558a6c30433d4d767
EC2 Default User <ec2-user@ip-172-31-32-178.ap-northeast-1.compute.internal>
2023-02-08 15:00:22 +0000

    gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :admins,skip: [:registrations, :passwords], controllers: {
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions:      'admin/sessions',
-    passwords:     'admin/passwords'
   }
-  devise_for :users,skip: [:passwords], controllers: {
+  devise_for :user,skip: [:passwords], controllers: {
     sessions:      'public/sessions',
-    passwords:     'public/passwords',
     registrations: 'public/registrations'
   }
-
-  root to: "public/homes#top"
 
   namespace :admin do
     resources :reviews, only: [:index, :edit, :destroy]
@@ -25,6 +21,6 @@ Rails.application.routes.draw do
     get 'homes/top'
   end
 
-
+  root to: "public/homes#top"
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
