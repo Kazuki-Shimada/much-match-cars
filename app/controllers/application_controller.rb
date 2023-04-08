9ce3bce8f8583e6ac47a10f7e4face99f68a8ecf
EC2 Default User <ec2-user@ip-172-31-32-178.ap-northeast-1.compute.internal>
2023-04-08 08:07:22 +0000

    javascript

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Admin
-      admin_reviews_path
+      admin_types_path
     when User
       user_path(current_user)
     end
