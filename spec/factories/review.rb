98798995dabd709086be49509782f4f515f4a997
EC2 Default User <ec2-user@ip-172-31-32-178.ap-northeast-1.compute.internal>
2023-06-22 12:42:29 +0000

    FactoryBot

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -1,13 +1,14 @@
-# FactoryBot.define do
-#   factory :book do
-#     sequence(:title) { |n| "title#{n}"}
-#     sequence(:body) { |n| "body#{n}"}
-#   end
-# end
-
 FactoryBot.define do
   factory :review do
     title { Faker::Lorem.characters(number: 10) }
     body { Faker::Lorem.characters(number: 30) }
+    rate {Faker::Number.between(to: 5)}
+    design {Faker::Number.between(to: 5)}
+    driving_performance {Faker::Number.between(to: 5)}
+    cost {Faker::Number.between(to: 5)}
+    size {Faker::Number.between(to: 5)}
+    safety {Faker::Number.between(to: 5)}
+    association :user
+    association :type
   end
 end
