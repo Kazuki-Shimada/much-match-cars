8849372ad1db81463f4e1fbcb0289ea74ee1d5cf
EC2 Default User <ec2-user@ip-172-31-32-178.ap-northeast-1.compute.internal>
2023-07-03 06:01:48 +0000

    finish[rspec]

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe 'Comments', type: :system do
   before do
     @review = create(:review)
-    @user = crate(:user)
+    @user = create(:user)
   end
   describe 'コメント投稿機能' do
     context 'コメントできるとき' do
@@ -18,7 +18,7 @@ RSpec.describe 'Comments', type: :system do
           }.to change { Comment.count }.by(1)
       end
     end
-    
+
     context 'コメントできないとき' do
       it "ログインしていないとコメントできない" do
         visit review_path(@review)
@@ -26,31 +26,41 @@ RSpec.describe 'Comments', type: :system do
       end
     end
   end
-  
+
   describe 'コメント削除機能' do
     before do
-      @user2 = create(:user)
       @comment = create(:comment)
-
     end
     context "コメントを削除できる時" do
       it "コメント主はコメントを削除できる" do
         sign_in (@comment.user)
         visit review_path(@comment.review)
+        expect(current_path).to eq review_path(@comment.review)
         expect{
-          find_link('削除', href: comment_path(@comment)).click
+          click_on '削除'
         }.to change { Comment.count }.by(-1)
         expect(page).to have_content 'コメントを削除しました'
         expect(page).to have_no_content("#{@comment.body}")
       end
       it "管理者はコメントを削除できる" do
+        @admin = create(:admin)
         sign_in @admin
+        visit admin_review_path(@comment.review)
+        expect(current_path).to eq admin_review_path(@comment.review)
+        expect{
+          click_on '削除'
+        }.to change { Comment.count }.by(-1)
+        expect(page).to have_content 'コメントを削除しました'
+        expect(page).to have_no_content("#{@comment.body}")
       end
     end
-    
-    const "コメントを削除できない時" do
+
+    context "コメントを削除できない時" do
       it "他のユーザーのコメントは削除できない" do
-        
+        @user = create(:user)
+        sign_in @user
+        visit review_path(@comment.review)
+        expect(page).to have_no_content("削除")
       end
     end
   end
